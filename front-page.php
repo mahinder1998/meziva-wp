7062fcf99d8ddb65b6ae2b5ab0bacb14d9436f29
mahinder1998 <mahinderkumar710@gmail.com>
2026-01-19 22:45:56 +0530

    Trust section compleetd

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@ if ( ! defined('ABSPATH') ) exit;
 
 get_header();
 get_template_part('template-parts/section-hero-banner');
+get_template_part('template-parts/section-trust_icons_section');
 get_template_part('template-parts/section-choose-shade');
 get_template_part('template-parts/section-about-product');
 get_template_part('template-parts/section-Ingredients');
