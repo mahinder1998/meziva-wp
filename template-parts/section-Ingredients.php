92eb6142c1c734d5b4a116f04a7f55ebc43f0d0e
mahinder1998 <mahinderkumar710@gmail.com>
2026-02-21 23:59:26 +0530

    ingredients section completed

--- template-parts/section-Ingredients.php
+++ template-parts/section-Ingredients.php
@@ -150,12 +150,12 @@ function mz_render_feature_icon($acf_icon, $classes = 'mz-w-full mz-h-full', $si
 
               <!-- ICON (ACF Image Upload) -->
               <?php if (!empty($icon_html)): ?>
-                <div class="mz-w-[60px] mz-h-[60px] mz-mx-auto md:mz-ml-0">
+                <div class="mz-w-[100px] mz-h-[60px] mz-mx-auto md:mz-ml-0">
                   <?php echo $icon_html; ?>
                 </div>
               <?php else: ?>
                 <!-- Optional fallback (remove if you want blank) -->
-                <div class="mz-w-[60px] mz-h-[60px] mz-mx-auto md:mz-ml-0 mz-opacity-50">
+                <div class="mz-w-[100px] mz-h-[60px] mz-mx-auto md:mz-ml-0 mz-opacity-50">
                   <div class="mz-w-full mz-h-full mz-rounded-full mz-bg-white/20"></div>
                 </div>
               <?php endif; ?>
